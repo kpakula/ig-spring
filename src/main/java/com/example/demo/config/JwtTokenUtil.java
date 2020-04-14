aa994f58546b2c627e0ae861418e37e7177e6505
Konrad Pakula <konrad.a.pakula@gmail.com>
2020-04-14 20:15:07 +0200

    Make token for subject

--- src/main/java/com/example/demo/config/JwtTokenUtil.java
+++ src/main/java/com/example/demo/config/JwtTokenUtil.java
@@ -2,10 +2,12 @@ package com.example.demo.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.Map;
 import java.util.function.Function;
 
 /**
@@ -51,6 +53,17 @@ public class JwtTokenUtil {
      */
     private Claims getAllClaimsFromToken(String token) {
         return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+    }
+
+
+    private String makeToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_EXPIRATION_TIME * 1000))
+                .signWith(SignatureAlgorithm.HS512, secret)
+                .compact();
     }
 
 }

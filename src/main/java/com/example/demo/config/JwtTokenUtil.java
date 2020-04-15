d6dc8b1e73c934de8c021cd4a5ffea5e84194f4d
Konrad Pakula <konrad.a.pakula@gmail.com>
2020-04-15 20:15:36 +0200

    Created generate token method

--- src/main/java/com/example/demo/config/JwtTokenUtil.java
+++ src/main/java/com/example/demo/config/JwtTokenUtil.java
@@ -4,9 +4,11 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
@@ -54,6 +56,12 @@ public class JwtTokenUtil {
     private Claims getAllClaimsFromToken(String token) {
         return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
     }
+
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        return makeToken(claims, userDetails.getUsername());
+    }
+
 
 
     private String makeToken(Map<String, Object> claims, String subject) {

c03cf23dbcada2a6c7aebfc56e6e1fb2a409266a
Konrad Pakula <konrad.a.pakula@gmail.com>
2020-04-27 21:13:34 +0200

    Created endpoint with user information

--- src/main/java/com/example/demo/controller/ProfileController.java
+++ src/main/java/com/example/demo/controller/ProfileController.java
@@ -1,13 +1,35 @@
 package com.example.demo.controller;
 
+import com.example.demo.config.JwtTokenUtil;
+import com.example.demo.model.UserDao;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.utils.Splitter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ProfileController {
 
-    @RequestMapping(value = "/profile", method = RequestMethod.POST)
-    public ResponseEntity<?> profile() {
-        return ResponseEntity.ok("Profile");
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private UserRepository repository;
+
+    @RequestMapping(value = "/profile", method = RequestMethod.GET)
+    public ResponseEntity<?> profile(@RequestHeader("Authorization") String token) {
+        String cleanToken = Splitter.getToken(token);
+        String username = jwtTokenUtil.getUsernameFromToken(cleanToken);
+        System.out.println(username);
+
+        UserDao userDao = repository.findByUsername(username);
+
+
+        return ResponseEntity.ok(userDao);
     }
+
+
+
+
 }
---
+++ src/main/java/com/example/demo/utils/Splitter.java
@@ -0,0 +1,7 @@
+package com.example.demo.utils;
+
+public class Splitter {
+    public static String getToken(String token) {
+        return token.substring(7);
+    }
+}
